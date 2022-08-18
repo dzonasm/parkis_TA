12be5cdce54d975dfbc4e772ba24eced5f971313
Deimantas Liubertas <deimantas.liubertas@halopowered.com>
2022-08-18 10:38:41 +0300

    chore

--- src/data/dental.data.ts
+++ src/data/dental.data.ts
@@ -1,29 +1,4 @@
 export const Data = {
   dentalTitle: 'Dental Insurance',
-  dentalDescription: 'Options for preventive care coverage and emergency or unplanned dental work - even special dental needs',
-  whatIsDentalTitle: 'What is Dental Insurance?',
-  whatIsDentalDescription:
-    'Dental insurance covers expenses specific to dental care, including both preventive care and emergency or unplanned dental work. Depending on the plan selected, it may also cover some special dental needs, such as orthodontia. Most plans will pay most if not all of your preventive care costs such as routine exams, x-rays, and fluoride treatments. For other dental costs such as fillings and extractions, you may pay for some or all of your expenses until a certain threshold is met per year, which will vary by plan.',
-  dentalWhoTypicallyTitle: 'Who typically needs it most?',
-  dentalFamiliesTitle: 'Families',
-  dentalFamiliesDescription:
-    'Children greatly benefit from regular care and dental care is even required by some school systems. In addition, carrying a dental policy with orthodontics coverage can save money for many families.',
-  dentalOlderPeopleTitle: 'Older people',
-  dentalOlderPeopleDescription:
-    'The older you are, the more likely you are to need extended dental services that can be high cost if paid out-of-pocket.',
-  dentalIssuesTitle: 'Dental issues',
-  dentalIssuesDescription: 'If you have experienced dental issues in the past, you may be more likely to need this type of coverage.',
-  dentalMercerInsightsTitle: 'Mercer Indigo Insights',
-  dentalFocusOnTitleAndDesc:
-    'Focus on prevention\n' +
-    '\n\n' +
-    'Keeping up on your dental visits can greatly reduce your need for major dental surgery as you age. Having a good dental plan that not only covers all of your preventive care - but can also handle those dental issues that will occasionally come up - will pay dividends in the long term and keep your mouth clean and healthy.',
-  dentalFocusOnDescription:
-    'To learn more about your options for dental coverage, please go to \'Find your product\' to discover which plans suit your needs based on your unique situation.',
-  dentalLetUsGuideTitle: 'Let us guide you to your perfect plan',
-  dentalLetUsGuideDescription: 'Tell us about yourself to customize your plan.',
-  dentalVideoTitle: 'Learn more about Dental Insurance',
-  dentalFindYourProductButton: 'Find your product',
-  dentalGetStartedTopButton: 'Get started',
-  dentalGetStartedBottomButton: 'Get started',
+  dentalDescription: 'Options for preventive care coverage and emergency or unplanned dental work - even special dental needs'
 };
--- src/data/vision.data.ts
+++ src/data/vision.data.ts
@@ -1,20 +1,4 @@
 export const Data = {
   visionTitle: 'Vision Insurance',
-  visionHeaderDescription: 'Eye exams, eyewear (glasses and contacts) and discounts on corrective surgeries',
-  whatIsVisionTitle: 'What is Vision Insurance?',
-  whatIsVisionDescription:
-    'For a low monthly cost, Vision Insurance can cover many of the expenses specific to vision care, including exams and eyewear (both eyeglasses and contact lenses). In most cases these plans offer discounts and/or limited coverage to reduce the overall cost of these expenses. This coverage may also allow for discounts on vision corrective surgeries, such as LASIK and PRK.',
-  visionMercerInsightsTitle: 'Mercer Indigo Insights',
-  visionTheSimpleDescription:
-    'The simple math of vision insurance\n\n' +
-    'Unlike some other types of insurance, you don\'t purchase vision insurance to keep you protected from a crisis or catastrophic event that could result in a heavy financial burden. However, vision insurance can be a good call to simply save you money on expenses that are a necessity for many people due to not-so-perfect vision. Vision expenses can be costly, so vision insurance at low monthly rates can cover some of the ongoing costs as they add up year over year.',
-  visionEncouragingDescription:
-    'Encouraging good vision health\n\n' +
-    '\n' +
-    'By covering most of the costs of a yearly check-up and replacement eyewear, there\'s no need to hesitate to have your eyes checked often, your prescription updated as needed, and generally keep on top of your eye health.',
-  visionLetUsGuideTitle: 'Let us guide you to your perfect plan',
-  visionLetUsGuideDescription: 'Tell us about yourself to customize your plan.',
-  visionFindYourProductButton: 'Find your product',
-  visionGetStartedTopButton: 'Get started',
-  visionGetStartedBottomButton: 'Get started',
+  visionHeaderDescription: 'Eye exams, eyewear (glasses and contacts) and discounts on corrective surgeries'
 };
