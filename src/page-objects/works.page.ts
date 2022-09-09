0c27c168cc3aaeff201880e720f4794ee3ec475f
unknown <pauliusb01@gmail.com>
2022-09-09 09:46:52 +0300

    tested main content

--- src/page-objects/works.page.ts
+++ src/page-objects/works.page.ts
@@ -1,5 +1,5 @@
 import urls from '../data/urls';
-import { getElement } from '../helpers/elements';
+import { getElement, getElementText, getNthElement } from '../helpers/elements';
 
 import { Page } from './page';
 
@@ -13,6 +13,78 @@ export default class works extends Page {
 
     get howItWorksHeaderTitleDescElem() {
         return getElement('[ta-id="how-it-works_851030490"]');
+    }
+
+    get howItWorksHeaderStartButtonIcon() {
+        return getElement('body > mercer-app > mercer-app-page-container > main > mercer-app-how-it-works > div > div.how-it-works__header.ng-star-inserted > div > div:nth-child(2) > div > brighter-icon > svg > use');
+    }
+
+    get howItWorksSubHeaderTitleElem() {
+        return getElement('[ta-id="how-it-works_987574216"]');
+    }
+
+    get howItWorksSubHeaderDescElem() {
+        return getElement('[ta-id="how-it-works_167268680"]');
+    }
+
+    get howItWorksSubHeaderImg() {
+        return getElement('[ta-id="how-it-works_731418922"]');
+    }
+
+    get howItWorksMainContentTitleElem() {
+        return getElement('[ta-id="how-it-works_534526354"]');
+    }
+
+    get howItWorksMainContentDescElem() {
+        return getElement('[ta-id="how-it-works_529035000"]');
+    }
+
+    get howItWorksMainContentFirstCardSubtileElem() {
+        return getElement('mercer-app-how-it-works > div > div:nth-child(3) > div > div.mos-u-color-bg--white.p-sm-5.md-p-md-3 > div:nth-child(3) > div > div > div > div:nth-child(1) > p');
+    }
+
+    // get howItWorksMainContentFirstCardIcon() {
+    //     return getNthElement('[ta-id="how-it-works_534363016"] brighter-icon > svg > use', 0);
+    // }
+
+    get howItWorksMainContentFirstCardIcon() {
+        return getElement('body > mercer-app > mercer-app-page-container > main > mercer-app-how-it-works > div > div:nth-child(3) > div > div.mos-u-color-bg--white.p-sm-5.md-p-md-3 > div:nth-child(3) > div > div > div > div:nth-child(1) > brighter-shape > div > brighter-icon > svg > use');
+    }
+
+    get howItWorksMainContentSecondCardSubtileElem() {
+        return getElement('body > mercer-app > mercer-app-page-container > main > mercer-app-how-it-works > div > div:nth-child(3) > div > div.mos-u-color-bg--white.p-sm-5.md-p-md-3 > div:nth-child(3) > div > div > div > div:nth-child(2) > p');
+    }
+
+    get howItWorksMainContentSecondCardIcon() {
+        return getElement('body > mercer-app > mercer-app-page-container > main > mercer-app-how-it-works > div > div:nth-child(3) > div > div.mos-u-color-bg--white.p-sm-5.md-p-md-3 > div:nth-child(3) > div > div > div > div:nth-child(2) > brighter-shape > div > brighter-icon > svg > use');
+    }
+
+    get howItWorksMainContentThirdCardSubtileElem() {
+        return getElement('body > mercer-app > mercer-app-page-container > main > mercer-app-how-it-works > div > div:nth-child(3) > div > div.mos-u-color-bg--white.p-sm-5.md-p-md-3 > div:nth-child(3) > div > div > div > div:nth-child(3) > p');
+    }
+
+    get howItWorksMainContentThirdCardIcon() {
+        return getElement('body > mercer-app > mercer-app-page-container > main > mercer-app-how-it-works > div > div:nth-child(3) > div > div.mos-u-color-bg--white.p-sm-5.md-p-md-3 > div:nth-child(3) > div > div > div > div:nth-child(3) > brighter-shape > div > brighter-icon > svg > use');
+    }
+
+    get howItWorksMainContentFourthCardSubtileElem() {
+        return getElement('body > mercer-app > mercer-app-page-container > main > mercer-app-how-it-works > div > div:nth-child(3) > div > div.mos-u-color-bg--white.p-sm-5.md-p-md-3 > div:nth-child(3) > div > div > div > div:nth-child(4) > p');
+    }
+
+    get howItWorksMainContentFourthCardIcon() {
+        return getElement('body > mercer-app > mercer-app-page-container > main > mercer-app-how-it-works > div > div:nth-child(3) > div > div.mos-u-color-bg--white.p-sm-5.md-p-md-3 > div:nth-child(3) > div > div > div > div:nth-child(4) > brighter-shape > div > brighter-icon > svg > use');
+    }
+
+    get howItWorksMainContentFifthCardSubtileElem() {
+        return getElement('body > mercer-app > mercer-app-page-container > main > mercer-app-how-it-works > div > div:nth-child(3) > div > div.mos-u-color-bg--white.p-sm-5.md-p-md-3 > div:nth-child(3) > div > div > div > div:nth-child(5) > p');
+    }
+
+    get howItWorksMainContentFifthCardIcon() {
+        return getElement('body > mercer-app > mercer-app-page-container > main > mercer-app-how-it-works > div > div:nth-child(3) > div > div.mos-u-color-bg--white.p-sm-5.md-p-md-3 > div:nth-child(3) > div > div > div > div:nth-child(5) > brighter-shape > div > brighter-icon > svg > use');
+    }
+
+    async howItWorksHeaderVideoElem(selector) {
+        return getElement(selector)
     }
 
 }
--- src/step-definitions/howItWorks.steps.ts
+++ src/step-definitions/howItWorks.steps.ts
@@ -1,8 +1,8 @@
 import { Then, When } from '@cucumber/cucumber';
 import { Context } from '../helpers/context';
 import urls from "../data/urls";
-import { clickElement, getElement, getNthElement } from "../helpers/elements";
-import { expectAttributeToContain, expectText, expectToExist, expectURL, expectValue } from "../helpers/validators";
+import { clickElement, getElement, getElementText, getNthElement } from "../helpers/elements";
+import { expectAttributeToContain, expectText, expectToExist, expectURL, expectValue, expectToContain } from "../helpers/validators";
 import Works from '../page-objects/works.page';
 
 const _Works: Works = Context.getInstance().getPage('Works');
@@ -14,6 +14,53 @@ When(/^the How it works page is opened$/, async () => {
 
 Then(/^check the following data of the header is correct$/, async (table) => {
     const howItWorksHeaderDataTable = await table.rowsHash();
+    await expectToExist(await _Works.howItWorksHeaderVideoElem(howItWorksHeaderDataTable.Video));
     await expectText(await _Works.howItWorksHeaderTitleElem, howItWorksHeaderDataTable.Title);
     await expectText(await _Works.howItWorksHeaderTitleDescElem, howItWorksHeaderDataTable.Description);
-   });
+    await expectAttributeToContain(await _Works.howItWorksHeaderStartButtonIcon, "xlink:href", howItWorksHeaderDataTable.StartButtonIcon);
+});
+
+Then(/^check the following data of the sub-header is correct$/, async (table) => {
+  const howItWorksSubHeaderDataTable = await table.rowsHash();
+  await expectText(await _Works.howItWorksSubHeaderTitleElem, howItWorksSubHeaderDataTable.Title);
+  await expectText(await _Works.howItWorksSubHeaderDescElem, howItWorksSubHeaderDataTable.Description);
+  await expectAttributeToContain(await _Works.howItWorksSubHeaderImg, "src", howItWorksSubHeaderDataTable.Picture);
+});
+
+Then(/^main content title and description should be correct$/, async (table) => {
+  const howItWorksMainContentDataTable = await table.rowsHash();
+  await expectText(await _Works.howItWorksMainContentTitleElem, howItWorksMainContentDataTable.Title);
+  await expectText(await _Works.howItWorksMainContentDescElem, howItWorksMainContentDataTable.Description);
+});
+
+Then(/^check the following data of main content first card is correct$/, async (table) => {
+  const howItWorksMainContentFirstCardDataTable = await table.rowsHash();
+  await expectText(await _Works.howItWorksMainContentFirstCardSubtileElem, howItWorksMainContentFirstCardDataTable.Subtitle);
+  await expectAttributeToContain(await _Works.howItWorksMainContentFirstCardIcon, "xlink:href", howItWorksMainContentFirstCardDataTable.Icon);
+});
+
+Then(/^check the following data of main content second card is correct$/, async (table) => {
+  const howItWorksMainContentSecondCardDataTable = await table.rowsHash();
+  await expectText(await _Works.howItWorksMainContentSecondCardSubtileElem, howItWorksMainContentSecondCardDataTable.Subtitle);
+  await expectAttributeToContain(await _Works.howItWorksMainContentSecondCardIcon, "xlink:href", howItWorksMainContentSecondCardDataTable.Icon);
+});
+
+Then(/^check the following data of main content third card is correct$/, async (table) => {
+  const howItWorksMainContentThirdCardDataTable = await table.rowsHash();
+  await expectText(await _Works.howItWorksMainContentThirdCardSubtileElem, howItWorksMainContentThirdCardDataTable.Subtitle);
+  await expectAttributeToContain(await _Works.howItWorksMainContentThirdCardIcon, "xlink:href", howItWorksMainContentThirdCardDataTable.Icon);
+});
+
+Then(/^check the following data of main content fourth card is correct$/, async (table) => {
+  const howItWorksMainContentFourthCardDataTable = await table.rowsHash();
+  await expectText(await _Works.howItWorksMainContentFourthCardSubtileElem, howItWorksMainContentFourthCardDataTable.Subtitle);
+  await expectAttributeToContain(await _Works.howItWorksMainContentFourthCardIcon, "xlink:href", howItWorksMainContentFourthCardDataTable.Icon);
+});
+
+Then(/^check the following data of main content fifth card is correct$/, async (table) => {
+  const howItWorksMainContentFifthCardDataTable = await table.rowsHash();
+  await expectText(await _Works.howItWorksMainContentFifthCardSubtileElem, howItWorksMainContentFifthCardDataTable.Subtitle);
+  await expectAttributeToContain(await _Works.howItWorksMainContentFifthCardIcon, "xlink:href", howItWorksMainContentFifthCardDataTable.Icon);
+});
+
+
